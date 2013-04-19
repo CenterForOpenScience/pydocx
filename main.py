f82ae051de8471b2779eb7dbf830db4a8d9c948e
Jason Ward <jason.ward@policystat.com>
2013-04-19 17:26:30 -0400

    Weird import error, not sure what is going on there. Found which tests were actually passing and stopped skipping them

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pydocx import *
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ElementTree
 #import lxml.etree as etree
 
 with open('test.html', 'w') as f:
     f.write(docx2html('helloworld.docx'))
-with open('testxml.html','w') as f:
-    f.write(BeautifulSoup(ElementTree.tostring(Docx2Html('helloworld.docx').root)).prettify())
+#with open('testxml.html','w') as f:
+#    f.write(BeautifulSoup(ElementTree.tostring(Docx2Html('helloworld.docx').root)).prettify())
 
 #print docx2html('helloworld.docx')
 #print docx2markdown('helloworld.docx')

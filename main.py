f695f54d042f804bff9d1aad3463d306fce0c8d1
Jason Ward <jason.ward@policystat.com>
2013-05-01 13:13:12 -0400

    refs #9: flake8 everything

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from pydocx import *
+from pydocx import docx2html, Docx2Html
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ElementTree
 
 
 with open('test.html', 'w') as f:
     f.write(docx2html('helloworld.docx'))
-with open('testxml.html','w') as f:
-    f.write(BeautifulSoup(ElementTree.tostring(Docx2Html('helloworld.docx').root)).prettify())
-
+with open('testxml.html', 'w') as f:
+    f.write(
+        BeautifulSoup(
+            ElementTree.tostring(
+                Docx2Html('helloworld.docx').root,
+            ),
+        ).prettify(),
+    )

ad5161437a7013179fddb066b9f9098e51ce8753
Jason Ward <jason.ward@policystat.com>
2013-05-15 16:50:38 -0400

    Do the pypi thing

---
+++ setup.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages  # noqa
+
+rel_file = lambda *args: os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), *args)
+
+
+def get_file(filename):
+    with open(rel_file(filename)) as f:
+        return f.read()
+
+
+def get_description():
+    return get_file('README.md')
+
+setup(
+    name="PyDocX",
+    version="0.1.1",
+    description="docx (OOXML) to html converter",
+    author="Jason Ward, Sam Portnow",
+    author_email="jason.louard.ward@gmail.com, samson91787@gmail.com",
+    url="http://github.com/OpenScienceFramework/pydocx",
+    platforms=["any"],
+    license="BSD",
+    packages=find_packages(),
+    scripts=[],
+    zip_safe=False,
+    install_requires=[],
+    cmdclass={},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+    ],
+    long_description=get_description(),
+)

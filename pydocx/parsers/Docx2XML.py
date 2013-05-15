aa1b66c2b35c8ebb8057a9349b81a58691f6df0f
Sam Portnow <samson91787@gmail.com>
2013-05-15 17:23:32 -0400

    added Docx2XML; made changes to htmlconverter

---
+++ pydocx/parsers/Docx2XML.py
@@ -0,0 +1,20 @@
+__author__ = 'samportnow'
+
+from pydocx.parsers.Docx2Html import Docx2Html
+import xml.sax.saxutils
+
+
+class Docx2XML(Docx2Html):
+
+    def insertion(self, text, author, date):
+        return (
+            "<insertion author='{author}' "
+            "date='{date}'>{text}</insertion>"
+            ).format(author=author, date=date, text=text)
+
+    def deletion(self, text, author, date):
+        return (
+            "<deletion author='{author}' "
+            "date='{date}'>{text}</deletion>"
+            ).format(author=author, date=date, text=text)
+

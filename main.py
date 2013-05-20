35ecab5779b1ec711971e0aa293ca42d8216ce52
Sam Portnow <samson91787@gmail.com>
2013-05-20 11:33:11 -0400

    got rid of unused import

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-from pydocx import docx2html, Docx2Html, HtmlConverter, docx2markdown, Docx2Markdown, Docx2XML, docx2xml, Docx2LaTex,\
-    docx2latex
-from pydocx.tests import test_xml
-from pydocx.tests import document_builder
-from pydocx.tests import XMLDocx2Html
-from bs4 import BeautifulSoup
-import os
-import xml.etree.ElementTree as ElementTree
-from pydocx.tests import (
-    ElementTree,
-    XMLDocx2Html,
-    _TranslationTestCase,
-    remove_namespaces,
-    )
+from pydocx import docx2html
 
-#print BeautifulSoup(
-#    ElementTree.tostring(
-#        Docx2Html('./pydocx/fixtures/simple_table.docx').root,
-#    ),
-#).prettify()
 print docx2html('./pydocx/fixtures/simple_table.docx')
 
 
@@ -25,10 +7,12 @@ print docx2html('./pydocx/fixtures/simple_table.docx')
 
 #print test_xml.TableTag(_TranslationTestCase).get_xml()
 
-#for (dirpath, dirnames, filenames) in os.walk('/Users/samportnow/Documents/pydocx/pydocx/fixtures'):
+#for (dirpath, dirnames, filenames)
+# in os.walk('/Users/samportnow/Documents/pydocx/pydocx/fixtures'):
 #    for fi in filenames:
 #        try:
 #            print 'THE TEST IS ' + fi
-#            print docx2markdown('/Users/samportnow/Documents/pydocx/pydocx/fixtures/'+fi)
+#            print docx2markdown
+# ('/Users/samportnow/Documents/pydocx/pydocx/fixtures/'+fi)
 #        except:
 #            print 'had to pass it'

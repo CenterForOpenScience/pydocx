0c618470238da132767bb4e90d02680bf0ca78ac
Sam Portnow <samson91787@gmail.com>
2013-05-20 19:52:26 -0400

    update

--- main.py
+++ main.py
@@ -8,6 +8,15 @@
 #        Docx2Html('./pydocx/fixtures/simple_table.docx').root,
 #    ),
 #).prettify()
+<<<<<<< .merge_file_TJcXXf
+=======
+print docx2html('./pydocx/fixtures/localDpi.docx')
+
+
+#print test_table.get_xml()
+
+#print test_xml.TableTag(_TranslationTestCase).get_xml()
+>>>>>>> .merge_file_ErTboO
 
 #with open('test.html', 'w') as f:
 #    f.write(docx2html('./helloworld.docx'))

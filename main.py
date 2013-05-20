ed45f6de2c9b69ccab5f8aed37a72213f168aa03
Sam Portnow <samson91787@gmail.com>
2013-05-20 10:45:33 -0400

    updated

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ElementTree
 
 
-with open('test.html', 'w') as f:
-    f.write(docx2html('./helloworld.docx'))
-with open('testxml.html', 'w') as f:
-    f.write(
-        BeautifulSoup(
-            ElementTree.tostring(
-                Docx2Html('./helloworld.docx').root,
-            ),
-        ).prettify(),
-    )
-print docx2html('./helloworld.docx')
 print BeautifulSoup(
     ElementTree.tostring(
-        Docx2Html('./helloworld.docx').root,
+        Docx2Html('./pydocx/fixtures/simple_table.docx').root,
     ),
 ).prettify()
+
+#with open('test.html', 'w') as f:
+#    f.write(docx2html('./helloworld.docx'))
+#with open('testxml.html', 'w') as f:
+#    f.write(
+#        BeautifulSoup(
+#            ElementTree.tostring(
+#                Docx2Html('./helloworld.docx').root,
+#            ),
+#        ).prettify(),
+#    )
+#print docx2html('./helloworld.docx')
+#print BeautifulSoup(
+#    ElementTree.tostring(
+#        Docx2Html('./helloworld.docx').root,
+#    ),
+#).prettify()

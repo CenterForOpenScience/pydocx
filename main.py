8d3e0699542f857894e7c91240afa8f52e715027
Sam Portnow <samson91787@gmail.com>
2013-05-20 10:22:32 -0400

    updating

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from pydocx import docx2html, Docx2Html
+from pydocx import docx2html, Docx2Html, docx2latex
 from bs4 import BeautifulSoup
+from pydocx.tests import document_builder
 import xml.etree.ElementTree as ElementTree
 
 
-with open('test.html', 'w') as f:
-    f.write(docx2html('./helloworld.docx'))
-with open('testxml.html', 'w') as f:
-    f.write(
-        BeautifulSoup(
-            ElementTree.tostring(
-                Docx2Html('./helloworld.docx').root,
-            ),
-        ).prettify(),
-    )
-print docx2html('./helloworld.docx')
-print BeautifulSoup(
-    ElementTree.tostring(
-        Docx2Html('./helloworld.docx').root,
-    ),
-).prettify()
+print docx2latex('./helloworld.docx')
+#print docx2html('./pydocx/fixtures/simple.docx')
+#print BeautifulSoup(
+#    ElementTree.tostring(
+#        Docx2Html('./pydocx/fixtures/simple.docx').root,
+#    ),
+#).prettify()
+

0abe0a30552eca57dc0d88cd7d0ae943002995af
Sam Portnow <samson91787@gmail.com>
2013-05-21 14:57:48 -0400

    updated main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 from pydocx.tests import document_builder
 import xml.etree.ElementTree as ElementTree
 
-print docx2latex('/Users/samportnow/Documents/motor-dediff-prod.docx')
+print docx2latex('/Users/samportnow/Documents/Nosek Articles/NHF2012.docx')
 
 with open('test.tex', 'w') as f:
-    f.write(docx2latex('/Users/samportnow/Documents/motor-dediff-prod.docx'))
+    f.write(docx2latex('/Users/samportnow/Documents/Nosek Articles/NHF2012.docx').encode('utf8'))
 #print BeautifulSoup(
 #    ElementTree.tostring(
 #        Docx2Html('./pydocx/fixtures/simple.docx').root,

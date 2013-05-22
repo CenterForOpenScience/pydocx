a43be1ab9841d847815f7de12502d34c5376e140
Sam Portnow <samson91787@gmail.com>
2013-05-22 18:48:00 -0400

    fixed some errors

--- pydocx/__init__.py
+++ pydocx/__init__.py
@@ -1,4 +1,13 @@
-from .parsers import Docx2LaTex
+from .parsers import Docx2LaTex, Docx2Html, Docx2Markdown
+
+
+def docx2html(path):
+    return Docx2Html(path).parsed
+
+
+def docx2markdown(path):
+    return Docx2Markdown(path).parsed
+
 
 def docx2latex(path):
     return Docx2LaTex(path).parsed

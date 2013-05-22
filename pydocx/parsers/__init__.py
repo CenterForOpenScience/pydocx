4937a55b354e768d7a80645abaa214bc1590f7de
Sam Portnow <samson91787@gmail.com>
2013-05-22 18:27:52 -0400

    updating

--- pydocx/parsers/__init__.py
+++ pydocx/parsers/__init__.py
@@ -1,4 +1,4 @@
 from pydocx.parsers.Docx2Html import Docx2Html
 from pydocx.parsers.Docx2Markdown import Docx2Markdown
-
+from pydocx.parsers.Docx2LaTex import Docx2LaTex
 __all__ = (Docx2Html, Docx2Markdown)

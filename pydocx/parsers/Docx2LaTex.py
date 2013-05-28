6b63d895ec3a985d28f401baa9cd34577daf3999
Sam Portnow <samson91787@gmail.com>
2013-05-28 16:34:16 -0400

    updating

--- pydocx/parsers/Docx2LaTex.py
+++ pydocx/parsers/Docx2LaTex.py
@@ -4,11 +4,7 @@ from pydocx.DocxParser import DocxParser
 class Docx2LaTex(DocxParser):
 
     def __init__(self, *args, **kwargs):
-        self.rows = 0
-        self.cols = 0
-        self.current_col = 0
-        self.table_info = []
-        self.columns = {}
+        self.table_info = {}
         super(Docx2LaTex, self).__init__(*args, **kwargs)
 
     @property
@@ -26,50 +22,50 @@ class Docx2LaTex(DocxParser):
         return text
 
     def linebreak(self):
-        return '\\\\ '
+        return '\n\n'
 
     def bold(self, text):
-        return '\\textbf {%s}' % text
+        return r'\textbf{%s}' % text
 
     def italics(self, text):
-        return '\\emph {%s}' % text
+        return r'\emph{%s}' % text
 
     def underline(self, text):
-        return '\\underline {%s}' % text
+        return r'\underline{%s}' % text
 
     def list_element(self, text):
-        return '\\item {%s}' % text
+        return r'\item %s' % text + '\n'
 
     def ordered_list(self, text, list_style):
-        return '\\begin{itemize}{%s}\\end{{enumerate}}' % text
+        return r'\begin{enumerate} %s \end{enumerate}' % text
 
     def unordered_list(self, text):
-        return '\\begin{itemize}{%s}\\end{{itemize}}' % text
+        return r'\begin{itemize} %s \end{itemize}' % text
 
     def head(self):
-        return "\\documentclass{article}\\usepackage{hyperref}"\
-               "\\usepackage{graphicx}\\usepackage{changes}" \
-               "\\usepackage{changepage} "\
-               "\\usepackage[paperwidth=%spt]{geometry}" \
-               "\\usepackage{hanging}" % self.page_width
+        return r'''\documentclass{article}\usepackage{hyperref}
+               \usepackage{graphicx}\usepackage{changes}
+               \usepackage{changepage}
+               \usepackage[paperwidth=%spt]{geometry}
+               \usepackage{hanging}\usepackage{multirow}''' % self.page_width
 
     def paragraph(self, text, pre=None):
-        return '\\par{'+text+'} '
+        return text + '\n\n'
 
     def heading(self, text, heading_value):
         #TODO figure out what to do for headings
         return text
 
     def insertion(self, text, author, date):
-        return '\\added[id='+author+',remark='+date+']{%s}' % text
+        return r'\added[id='+author+',remark='+date+']{%s}' % text
 
     def hyperlink(self, text, href):
         if text == '':
             return ''
-        return '\\href{%(href)s}{%(text)s}' % {
+        return r'\href{%(href)s}{%(text)s}' % {
             'href': href,
             'text': text,
-        }
+            }
 
     def image_handler(self, path):
         return path
@@ -85,12 +81,12 @@ class Docx2LaTex(DocxParser):
             y = float(y)
             x = x * float(3) / float(4)
             y = y * float(3) / float(4)
-            return '\\includegraphics[height=%spt, width=%spt] {%s}' % (
+            return r'\includegraphics[height=%spt, width=%spt]{%s}' % (
                 y,
                 x,
                 src)
         else:
-            return '\\includegraphics {%s}' % src
+            return r'\includegraphics {%s}' % src
 
     def tab(self):
         return '\\qquad '
@@ -99,7 +95,6 @@ class Docx2LaTex(DocxParser):
         center = False
         right = False
         setup_cols = ''
-        print self.cols
         for i in range(self.cols):
             for column in self.table_info:
                 if column['Column'] == i and column['justify'] == 'center':
@@ -115,77 +110,89 @@ class Docx2LaTex(DocxParser):
             else:
                 setup_cols += 'l'
         self.table_info = []
-        return '\\\\\\begin{tabular} {%s} %s \\end{tabular}\\\\ ' \
-               % (setup_cols, text)
+        return r'\begin{tabular}{%s}' % setup_cols\
+               + '\n'+ r'%s\end{tabular}'\
+               % text + '\n'
 
     def table_row(self, text):
-        return '%s \\\\ ' % text
+        return text
 
     def table_cell(self, text, col='', row=''):
+        slug = ''
+        if col:
+            slug += r'\multicolumn{%s}{*}' % col
+        if row:
+            slug += r'\multirow{%s}{*}' % row
+        if self.line_break_in_table:
+            slug += r'\pbox{20cm}{' + text + '}'
+        else:
+            slug += text
+        if self.last_row_item:
+            slug += r' \\' + '\n'
+            return slug
+        self.line_break_in_table = False
         return '%s & ' % text
 
     def page_break(self):
-        return '\\newpage '
-
-    def indent_table(self, just='', firstLine='', left='', right='', column=0):
-        self.columns = {}
-        self.columns['Column'] = column
-        self.columns['justify'] = just
-        if self.columns not in self.table_info:
-            self.table_info.append(self.columns)
-        return ''
+        return r'\newpage '
 
     def indent(self, text, just='', firstLine='',
-               left='', right='', hanging=0):
-        raggedright = False
-        raggedleft = False
-        center = False
-        slug = '{'
-        if hanging:
-            slug += '\\begin{hangpara}{%spt}{1} ' % (hanging)
-        if left and not right:
-            left = float(left)
-            left = left * float(3) / float(4)
-            slug += '\\begin{adjustwidth}{}{%spt}' % (left)
-        if right and not left:
-            right = float(right)
-            right = right * float(3) / float(4)
-            slug += '\\begin{adjustwidth}{%spt}{}' % (right)
-        if right and left:
-            left = float(left)
-            right = float(right)
-            left = left * float(3) / float(4)
-            right = right * float(3) / float(4)
-            slug += '\\begin{adjustwidth}{%spt}{%spt}' % (left, right)
-        if firstLine:
-            slug += '\\setlength{\\parindent}{'+firstLine+'pt}\\indent '
-        if just:
-            if just == 'left':
-                raggedright = True
-                slug += '\\begin{flushright} '
-            elif just == 'center':
-                center = True
-                slug += '\\begin{center} '
-            elif just == 'right':
-                raggedleft = True
-                slug += '\\begin{flushleft} '
-        slug += text
-        if left or right:
-            slug += '\\end{adjustwidth}'
-        if hanging:
-            slug += '\\end{hangpara}'
-        if raggedright:
-            slug += '\\end{flushright}'
-        if center:
-            slug += '\\end{center}'
-        if raggedleft:
-            slug += '\\end{flushleft}'
-        slug += '}'
-        return slug
-        #TODO left and right
+               left='', right='', hanging= ''):
+        if not self.indent_table:
+            raggedright = False
+            raggedleft = False
+            center = False
+            slug = ''
+            if hanging:
+                return r'\begin{hangparas}{%spt}{1} %s \end{hangparas}' % (hanging, text)
+            if right and left:
+                left = float(left)
+                right = float(right)
+                left = left * float(3) / float(4)
+                right = right * float(3) / float(4)
+                slug += r'\begin{adjustwidth}{%spt}{%spt}' % (left, right)
+            elif left:
+                left = float(left)
+                left = left * float(3) / float(4)
+                slug += r'\begin{adjustwidth}{}{%spt}' % (left)
+            elif right:
+                right = float(right)
+                right = right * float(3) / float(4)
+                slug += r'\begin{adjustwidth}{%spt}{}' % (right)
+            if firstLine:
+                slug += r'\setlength{\parindent}{'+firstLine+r'pt}\indent '
+            if just:
+                if just == 'left':
+                    raggedright = True
+                    slug += r'\begin{flushright} '
+                elif just == 'center':
+                    center = True
+                    slug += r'\begin{center} '
+                elif just == 'right':
+                    raggedleft = True
+                    slug += r'\begin{flushleft} '
+            slug += text
+            if left or right:
+                slug += r'\end{adjustwidth}'
+            if raggedright:
+                slug += r'\end{flushright}'
+            if center:
+                slug += r'\end{center}'
+            if raggedleft:
+                slug += r'\end{flushleft}'
+            return slug
+        else:
+            self.columns = {}
+            self.columns['Column'] = self.column_index
+            self.columns['justify'] = just
+            if self.columns not in self.table_info:
+                self.table_info.append(self.columns)
+            return ''
 
     def break_tag(self):
-        return ''
+        if self.is_table:
+            self.line_break_in_table = True
+        return r'\\'
 
     def deletion(self, text, author, date):
-        return '\\deleted[id='+author+',remark='+date+']{%s}' % text
+        return r'\deleted[id='+author+',remark='+date+']{%s}' % text

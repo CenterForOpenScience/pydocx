18213fbf12197858b93bdfc795e1f5a691e3caa6
Sam Portnow <samson91787@gmail.com>
2013-06-03 18:02:14 -0400

    fixed init

--- pydocx/__init__.py
+++ pydocx/__init__.py
@@ -9,9 +9,7 @@ def docx2markdown(path):
     return Docx2Markdown(path).parsed
 
 
-<<<<<<< HEAD
 def docx2latex(path):
     return Docx2LaTex(path).parsed
-=======
+
 VERSION = '0.2.1'
->>>>>>> a0f1daa2821285ba65a44bd30e2f2bcd19d17791

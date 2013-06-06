6a28b21a3ee30a5553a71feab2772c3429756531
Sam Portnow <samson91787@gmail.com>
2013-06-06 11:57:06 -0400

    merged with master

--- pydocx/__init__.py
+++ pydocx/__init__.py
@@ -1,5 +1,5 @@
 from .parsers import Docx2Html, Docx2Markdown
-
+from HtmlConverter import Html2Docx
 
 def docx2html(path):
     return Docx2Html(path).parsed
@@ -7,6 +7,9 @@ def docx2html(path):
 
 def docx2markdown(path):
     return Docx2Markdown(path).parsed
+
+def html2docx(path):
+    return Html2Docx(path).parsed
 
 
 VERSION = '0.2.1'

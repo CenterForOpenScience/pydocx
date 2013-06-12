bc28d22ac7d4c4709150089ce82efa539520279b
Sam Portnow <samson91787@gmail.com>
2013-06-12 14:46:31 -0400

    removed uncessary import

--- pydocx/__init__.py
+++ pydocx/__init__.py
@@ -1,5 +1,4 @@
 from .parsers import Docx2Html, Docx2Markdown
-from HtmlConverter import Html2Docx
 
 def docx2html(path):
     return Docx2Html(path).parsed
@@ -7,9 +6,5 @@ def docx2html(path):
 
 def docx2markdown(path):
     return Docx2Markdown(path).parsed
-
-def html2docx(path):
-    return Html2Docx(path).parsed
-
 
 VERSION = '0.3.0'

8e9bc9805d911546a2364f3f21382eb3b84917e7
Sam Portnow <samson91787@gmail.com>
2013-06-17 16:53:09 -0400

    updated changelog

--- pydocx/DocxParser.py
+++ pydocx/DocxParser.py
@@ -183,7 +183,7 @@ class DocxParser:
     def parse_table_cell(self, el, text):
         v_merge = find_first(el, 'vMerge')
         if v_merge is not None and (
-            'restart' != v_merge.get('val', '')):
+                'restart' != v_merge.get('val', '')):
             return ''
         colspan = self.get_colspan(el)
         rowspan = self._get_rowspan(el, v_merge)

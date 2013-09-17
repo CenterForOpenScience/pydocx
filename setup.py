e00173efc2305c1000f09e48f4f4d3528f211bff
Jason Ward <jason.ward@policystat.com>
2013-09-17 10:27:16 -0400

    Bumped to version 0.3.11

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_description():
 setup(
     name="PyDocX",
     # Edit here and pydocx.__init__
-    version="0.3.10",
+    version="0.3.11",
     description="docx (OOXML) to html converter",
     author="Jason Ward, Sam Portnow",
     author_email="jason.louard.ward@gmail.com, samson91787@gmail.com",

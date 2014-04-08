f0895184ecc682a20e31a48f6b88ac387f0402ef
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-04-08 01:51:17 -0400

    Refs #77. Added StyleDefinitionsPart and ChildPartLoader abstraction

--- pydocx/wordml.py
+++ pydocx/wordml.py
@@ -8,7 +8,44 @@ from pydocx.openxml import (
 from pydocx.utils import zip_path_join
 
 
-class MainDocumentPart(OpenXmlPart):
+class ChildPartLoader(object):
+    child_part_types = NotImplemented
+
+    def get_relationship_lookup(self):
+        raise NotImplementedError
+
+    def load_parts(self):
+        relationship_lookup = self.get_relationship_lookup()
+        for child_part_type in self.child_part_types:
+            relationships = relationship_lookup.get_relationships_by_type(
+                child_part_type.relationship_type,
+            )
+            relationship = relationships[0]
+            part_uri = zip_path_join(
+                relationship.source_uri,
+                relationship.target_uri,
+            )
+            part = child_part_type(
+                open_xml_package=self,
+                uri=part_uri,
+            )
+            self.add_part(
+                part=part,
+                relationship_id=relationship.relationship_id,
+            )
+
+
+class StyleDefinitionsPart(OpenXmlPart):
+    relationship_type = '/'.join([
+        'http://schemas.openxmlformats.org',
+        'officeDocument',
+        '2006',
+        'relationships',
+        'styles',
+    ])
+
+
+class MainDocumentPart(ChildPartLoader, OpenXmlPart):
     relationship_type = '/'.join([
         'http://schemas.openxmlformats.org',
         'officeDocument',
@@ -17,9 +54,19 @@ class MainDocumentPart(OpenXmlPart):
         'officeDocument',
     ])
 
+    child_part_types = [
+        StyleDefinitionsPart,
+    ]
+
+    def get_relationship_lookup(self):
+        return self.package.get_part(self.uri)
+
     @property
     def style_definitions_part(self):
-        pass
+        self.ensure_parts_are_loaded()
+        return self.get_parts_of_type(
+            StyleDefinitionsPart.relationship_type,
+        )[0]
 
     @property
     def numbering_definitions_part(self):
@@ -30,7 +77,7 @@ class MainDocumentPart(OpenXmlPart):
         pass
 
 
-class WordprocessingDocument(OpenXmlPackage):
+class WordprocessingDocument(ChildPartLoader, OpenXmlPackage):
     namespace = '/'.join([
         'http://schemas.openxmlformats.org',
         'wordprocessingml',
@@ -38,33 +85,19 @@ class WordprocessingDocument(OpenXmlPackage):
         'main',
     ])
 
-    def __init__(self, path):
-        super(WordprocessingDocument, self).__init__(path=path)
-        self._main_document_part = None
+    child_part_types = [
+        MainDocumentPart,
+    ]
+
+    def get_relationship_lookup(self):
+        return self.package
 
     @property
     def main_document_part(self):
-        if self._main_document_part is None:
-            self.load_parts()
-        return self._main_document_part
-
-    def load_parts(self):
-        relationships = self.package.get_relationships_by_type(
+        self.ensure_parts_are_loaded()
+        return self.get_parts_of_type(
             MainDocumentPart.relationship_type,
-        )
-        relationship = relationships[0]
-        part_uri = zip_path_join(
-            relationship.source_uri,
-            relationship.target_uri,
-        )
-        self._main_document_part = MainDocumentPart(
-            open_xml_package=self,
-            uri=part_uri,
-        )
-        self.add_part(
-            part=self._main_document_part,
-            relationship_id=relationship.relationship_id,
-        )
+        )[0]
 
 
 class Document(object):

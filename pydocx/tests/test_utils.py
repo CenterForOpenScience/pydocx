8d4311c4aa5392b360c653016ca713adba42d614
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-04-24 17:37:59 -0400

    Refs #85. Added a test case on find_first

--- pydocx/tests/test_utils.py
+++ pydocx/tests/test_utils.py
@@ -1,9 +1,9 @@
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 from unittest import TestCase
 from xml.etree import cElementTree
 
-from pydocx.utils import el_iter
+from pydocx.utils import el_iter, find_first
 
 
 def elements_to_tags(elements):
@@ -20,3 +20,17 @@ class UtilsTestCase(TestCase):
         result = el_iter(root)
 
         self.assertEqual(list(elements_to_tags(result)), expected)
+
+    def test_find_first(self):
+        xml = b'<?xml version="1.0" ?><one><two><three/></two></one>'
+        root = cElementTree.fromstring(xml)
+
+        # Can't find the root element
+        result = find_first(root, 'one')
+        self.assertEqual(result, None)
+
+        result = find_first(root, 'three')
+        self.assertEqual(result.tag, 'three')
+
+        result = find_first(root, 'two')
+        self.assertEqual(result.tag, 'two')

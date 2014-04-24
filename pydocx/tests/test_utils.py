a58de572f976b9accd0510ad0798e6cd761fc6a2
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-04-24 17:55:25 -0400

    Refs #85. Added test case for find_all

--- pydocx/tests/test_utils.py
+++ pydocx/tests/test_utils.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from unittest import TestCase
 from xml.etree import cElementTree
 
-from pydocx.utils import el_iter, find_first
+from pydocx.utils import el_iter, find_all, find_first
 
 
 def elements_to_tags(elements):
@@ -11,10 +11,14 @@ def elements_to_tags(elements):
         yield element.tag
 
 
+def make_xml(s):
+    xml = b'<?xml version="1.0"?>' + s
+    return cElementTree.fromstring(xml)
+
+
 class UtilsTestCase(TestCase):
     def test_el_iter(self):
-        xml = b'<?xml version="1.0" ?><one><two><three/><three/></two></one>'
-        root = cElementTree.fromstring(xml)
+        root = make_xml(b'<one><two><three/><three/></two></one>')
 
         expected = ['one', 'two', 'three', 'three']
         result = el_iter(root)
@@ -22,10 +26,7 @@ class UtilsTestCase(TestCase):
         self.assertEqual(list(elements_to_tags(result)), expected)
 
     def test_find_first(self):
-        xml = b'''<?xml version="1.0" ?>
-            <one><two><three v="1"/><three v="2"/></two></one>
-        '''
-        root = cElementTree.fromstring(xml)
+        root = make_xml(b'<one><two><three v="1"/><three v="2"/></two></one>')
 
         # Can't find the root element
         result = find_first(root, 'one')
@@ -37,3 +38,17 @@ class UtilsTestCase(TestCase):
 
         result = find_first(root, 'two')
         self.assertEqual(result.tag, 'two')
+
+    def test_find_all(self):
+        root = make_xml(b'<one><two><three/><three/></two></one>')
+
+        # Can't find the root element
+        result = find_all(root, 'one')
+        self.assertEqual(result, [])
+
+        result = find_all(root, 'three')
+        expected = ['three', 'three']
+        self.assertEqual(list(elements_to_tags(result)), expected)
+
+        result = find_all(root, 'two')
+        self.assertEqual(list(elements_to_tags(result)), ['two'])

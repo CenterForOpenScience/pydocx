e36d4a9f6bead8acbb40d18a633c1129ca8d7e6d
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-04-25 00:11:19 -0400

    Refs #85. __all__ should be a list of strings

--- pydocx/parsers/__init__.py
+++ pydocx/parsers/__init__.py
@@ -1,4 +1,7 @@
 from pydocx.parsers.Docx2Html import Docx2Html
 from pydocx.parsers.Docx2Markdown import Docx2Markdown
 
-__all__ = (Docx2Html, Docx2Markdown)
+__all__ = [
+    'Docx2Html',
+    'Docx2Markdown',
+]

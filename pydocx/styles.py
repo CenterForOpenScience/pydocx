230e0818dfa3b699119413cc64165c40a6103e4f
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-06-03 16:56:36 -0400

    Refs #71. Handle processing rPr as RunProperties

--- pydocx/styles.py
+++ pydocx/styles.py
@@ -4,25 +4,49 @@ from __future__ import (
     unicode_literals,
 )
 
+from collections import defaultdict
+
+from pydocx.types import OnOff
+
+
+class RunProperties(object):
+    def __init__(self, bold=False):
+        self.bold = bool(OnOff(bold))
+
+    @staticmethod
+    def load(element):
+        bold = False
+        for child in element:
+            if child.tag == 'b' and not bold:
+                bold = child.attrib.get('val', '')
+        return RunProperties(
+            bold=bold,
+        )
+
 
 class Style(object):
-    def __init__(self, style_id, style_type, name):
+    def __init__(self, style_id, style_type, name, run_properties):
         self.style_id = style_id
         self.style_type = style_type
         self.name = name
+        self.run_properties = run_properties
 
     @staticmethod
     def load(element):
         style_type = element.attrib.get('type', 'paragraph')
         style_id = element.attrib.get('styleId', '')
         name = ''
+        run_properties = None
         for child in element:
             if child.tag == 'name' and not name:
                 name = child.attrib.get('val', '')
+            if child.tag == 'rPr' and not run_properties:
+                run_properties = RunProperties.load(child)
         return Style(
             style_id=style_id,
             style_type=style_type,
             name=name,
+            run_properties=run_properties,
         )
 
 

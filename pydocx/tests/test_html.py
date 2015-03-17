f28bfd09afd5af97c0c9ae4856c73591f67fe217
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-03-17 15:33:31 -0400

    Refs #112. Added test cases

--- pydocx/tests/test_html.py
+++ pydocx/tests/test_html.py
@@ -6,6 +6,8 @@ from __future__ import (
     unicode_literals,
 )
 
+from nose import SkipTest
+
 from pydocx.tests import (
     DocumentGeneratorTestCase,
     WordprocessingDocumentFactory,
@@ -16,6 +18,76 @@ from pydocx.wordml import (
     MainDocumentPart,
     StyleDefinitionsPart,
 )
+
+
+class XMLVulnerabilitiesTestCase(DocumentGeneratorTestCase):
+    def test_exponential_entity_expansion(self):
+        try:
+            import defusedxml
+        except ImportError:
+            defusedxml = None
+
+        if defusedxml is None:
+            raise SkipTest('This test case only applies when using defusedxml')
+
+        document_xml = '''
+            <p>
+              <r>
+                <t>&c;</t>
+              </r>
+            </p>
+        '''
+        xml_header = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE xml [
+ <!ENTITY a "123">
+ <!ENTITY b "&a;&a;">
+ <!ENTITY c "&b;&b;">
+]>
+        '''
+        document = WordprocessingDocumentFactory(xml_header=xml_header)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '<p>123123123123</p>'
+        try:
+            self.assert_document_generates_html(document, expected_html)
+            raise AssertionError(
+                'Expected "EntitiesForbidden" exception did not occur',
+            )
+        except defusedxml.EntitiesForbidden:
+            pass
+
+    def test_entity_blowup(self):
+        try:
+            import defusedxml
+        except ImportError:
+            defusedxml = None
+
+        if defusedxml is None:
+            raise SkipTest('This test case only applies when using defusedxml')
+
+        document_xml = '''
+            <p>
+              <r>
+                <t>&a;</t>
+              </r>
+            </p>
+        '''
+        xml_header = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE xml [
+ <!ENTITY a "123">
+]>
+        '''
+        document = WordprocessingDocumentFactory(xml_header=xml_header)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '<p>123</p>'
+        try:
+            self.assert_document_generates_html(document, expected_html)
+            raise AssertionError(
+                'Expected "EntitiesForbidden" exception did not occur',
+            )
+        except defusedxml.EntitiesForbidden:
+            pass
 
 
 class FootnoteTestCase(DocumentGeneratorTestCase):

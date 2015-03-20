5109235104bd5a046d15cce926b62eb5dde0d625
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-03-20 15:52:17 -0400

    Refs #115. More test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,8 +4,13 @@ from __future__ import (
     unicode_literals,
 )
 
+from tempfile import NamedTemporaryFile
 from unittest import TestCase
 
+from nose import SkipTest
+
+from pydocx.test.testcases import BASE_HTML
+from pydocx.test.utils import assert_html_equal
 from pydocx.__main__ import main
 
 
@@ -25,3 +30,37 @@ class MainTestCase(TestCase):
     def test_return_code_with_three_args(self):
         result = main(['foo', 'bar', 'baz'])
         self.assertEqual(result, 2)
+
+    def test_convert_to_html_status_code(self):
+        with NamedTemporaryFile() as f:
+            result = main([
+                '--html',
+                'tests/fixtures/inline_tags.docx',
+                f.name,
+            ])
+        self.assertEqual(result, 0)
+
+    def test_convert_to_markdown_status_code(self):
+        with NamedTemporaryFile() as f:
+            result = main([
+                '--markdown',
+                'tests/fixtures/inline_tags.docx',
+                f.name,
+            ])
+        self.assertEqual(result, 0)
+
+    def test_convert_to_html_result(self):
+        fixture_html = open('tests/fixtures/inline_tags.html').read()
+        expected_html = BASE_HTML % fixture_html
+        with NamedTemporaryFile() as f:
+            result = main([
+                '--html',
+                'tests/fixtures/inline_tags.docx',
+                f.name,
+            ])
+            data = open(f.name).read()
+            assert_html_equal(data, expected_html)
+        self.assertEqual(result, 0)
+
+    def test_convert_to_markdown_result(self):
+        raise SkipTest('Fixture files for markdown do not exist yet')

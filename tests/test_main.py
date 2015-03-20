7dc2de2908bad4db015869fb2d1cc6b4d82f4c6a
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-03-20 15:33:36 -0400

    Refs #115. More test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,18 @@ from pydocx.__main__ import main
 
 
 class MainTestCase(TestCase):
-    def test_return_code_with_no_arguments(self):
+    def test_return_code_with_no_args(self):
         result = main()
         self.assertEqual(result, 1)
+
+    def test_return_code_with_one_args(self):
+        result = main(['foo'])
+        self.assertEqual(result, 1)
+
+    def test_return_code_with_two_args(self):
+        result = main(['foo', 'bar'])
+        self.assertEqual(result, 1)
+
+    def test_return_code_with_three_args(self):
+        result = main(['foo', 'bar', 'baz'])
+        self.assertEqual(result, 2)

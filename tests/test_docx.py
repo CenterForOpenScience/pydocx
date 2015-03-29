564677d89c23b5377af4d06329f6fd8323afbffa
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-03-29 13:16:00 -0400

    Refs #116. The faked subscript fixture test case is not needed anymore

--- tests/test_docx.py
+++ tests/test_docx.py
@@ -45,7 +45,6 @@ class ConvertDocxToHtmlTestCase(TestCase):
         # level upperRomans are converted.
         'external_image',
         'export_from_googledocs',
-        'fake_subscript',
         'has_missing_image',
         'list_to_header',
         'lists_with_styles',

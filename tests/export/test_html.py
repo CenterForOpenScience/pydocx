1c53737a95d53c753ccff090acaec676ac1f1cb0
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-04-06 16:45:54 -0400

    Refs #121. Added a failing test case

--- tests/export/test_html.py
+++ tests/export/test_html.py
@@ -10,6 +10,7 @@ from pydocx.wordml import (
     FootnotesPart,
     ImagePart,
     MainDocumentPart,
+    NumberingDefinitionsPart,
     StyleDefinitionsPart,
 )
 
@@ -1677,4 +1678,42 @@ class HyperlinkTestCase(DocumentGeneratorTestCase):
         document.add(MainDocumentPart, document_xml, document_rels)
 
         expected_html = '<p><a href="http://google.com">li<br />nk</a>.</p>'
+        self.assert_document_generates_html(document, expected_html)
+
+
+class NumberingTestCase(DocumentGeneratorTestCase):
+    def test_lowerLetter_numbering_format_is_handled(self):
+        num_id = '2'
+        numbering_xml = '''
+            <num numId="{num_id}">
+                <abstractNumId val="1"/>
+            </num>
+            <abstractNum abstractNumId="1">
+                <lvl ilvl="0">
+                    <numFmt val="lowerLetter"/>
+                </lvl>
+            </abstractNum>
+        '''.format(num_id=num_id)
+
+        document_xml = '''
+            <p>
+                <pPr>
+                    <numPr>
+                        <ilvl val="0" />
+                        <numId val="{num_id}" />
+                    </numPr>
+                </pPr>
+                <r><t>AAA</t></r>
+            </p>
+        '''.format(num_id=num_id)
+
+        document = WordprocessingDocumentFactory()
+        document.add(NumberingDefinitionsPart, numbering_xml)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '''
+            <ol list-style-type="lowerLetter">
+                <li>AAA</li>
+            </ol>
+        '''
         self.assert_document_generates_html(document, expected_html)

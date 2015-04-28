7d7cc1e8eaf9d48aa5b58330a463844c25f2814d
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-04-28 11:32:53 -0400

    Refs #129. Added test cases for Numbering

---
+++ tests/openxml/wordprocessing/test_numbering.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+)
+
+from unittest import TestCase
+
+from pydocx.openxml.wordprocessing import (
+    AbstractNum,
+    Numbering,
+    NumberingInstance,
+)
+from pydocx.util.xml import parse_xml_from_string
+
+
+class NumberingTestCase(TestCase):
+    def _load_from_xml(self, xml):
+        root = parse_xml_from_string(xml)
+        return Numbering.load(root)
+
+    def test_numbering_elements_includes_abstractNum_and_num_ordered(self):
+        xml = b'''
+            <numbering>
+                <abstractNum abstractNumId="1">
+                    <name val="foo"/>
+                </abstractNum>
+                <num numId="1">
+                    <abstractNumId val="1" />
+                </num>
+            </numbering>
+        '''
+        numbering = self._load_from_xml(xml)
+        expected_classes = [
+            AbstractNum,
+            NumberingInstance,
+        ]
+        classes = [
+            element.__class__
+            for element in numbering.elements
+        ]
+        self.assertEqual(classes, expected_classes)

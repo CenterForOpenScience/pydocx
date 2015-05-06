f7147e2a56f86c31b9cba8a57da255bf4855075f
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-06 15:47:36 -0400

    Refs #136. Implemented get_numbering_definition

--- pydocx/openxml/wordprocessing/numbering.py
+++ pydocx/openxml/wordprocessing/numbering.py
@@ -14,3 +14,27 @@ class Numbering(XmlModel):
     XML_TAG = 'numbering'
 
     elements = XmlCollection(AbstractNum, NumberingInstance)
+
+    def __init__(self, **kwargs):
+        super(Numbering, self).__init__(**kwargs)
+
+        self._abstract_nums = {}
+        self._nums = {}
+
+        for el in self.elements:
+            if isinstance(el, AbstractNum):
+                self._abstract_nums[el.abstract_num_id] = el
+            elif isinstance(el, NumberingInstance):
+                self._nums[el.num_id] = el
+            else:
+                raise AssertionError(
+                    'Unexpected element type {type} encountered'.format(
+                        type=el.__class__.__name__,
+                    )
+                )
+
+    def get_numbering_definition(self, num_id):
+        num = self._nums.get(num_id)
+        if not num:
+            return
+        return self._abstract_nums.get(num.abstract_num_id)

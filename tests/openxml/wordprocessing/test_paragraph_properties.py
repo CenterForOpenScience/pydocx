a15f276e090d93aae96d8233001bb130f6a913d0
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-06 15:22:10 -0400

    Refs #136. Added missing test case for paragraph properties

---
+++ tests/openxml/wordprocessing/test_paragraph_properties.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+)
+
+from unittest import TestCase
+
+from pydocx.openxml.wordprocessing import NumberingProperties, ParagraphProperties  # noqa
+from pydocx.util.xml import parse_xml_from_string
+
+
+class ParagraphPropertiesTestCase(TestCase):
+    def _load_from_xml(self, xml):
+        root = parse_xml_from_string(xml)
+        return ParagraphProperties.load(root)
+
+    def test_pStyle_child_mapped_to_parent_style_attribute(self):
+        xml = '''
+            <pPr>
+                <pStyle val="foostyle" />
+            </pPr>
+        '''
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.parent_style, "foostyle")

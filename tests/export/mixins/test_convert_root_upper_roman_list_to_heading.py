2165e22350c1c1c4ddeb7e60679f4261ea44908a
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-07 13:28:37 -0400

    Refs #136. Refactor + added several additional test cases

--- tests/export/mixins/test_convert_root_upper_roman_list_to_heading.py
+++ tests/export/mixins/test_convert_root_upper_roman_list_to_heading.py
@@ -32,32 +32,31 @@ class ConvertRootUpperRomanListToHeadingExporter(
 
 class ConvertRootUpperRomanListToHeadingTestCase(DocumentGeneratorTestCase):
     exporter = ConvertRootUpperRomanListToHeadingExporterNoStyle
+    list_item = '''
+        <p>
+          <pPr>
+            <numPr>
+              <ilvl val="{level}"/>
+              <numId val="{num_id}"/>
+            </numPr>
+          </pPr>
+          <r><t>{content}</t></r>
+        </p>
+    '''
 
-    def test_only_root_level_upper_roman_list_converted_to_heading(self):
-        list_item = '''
-            <p>
-              <pPr>
-                <numPr>
-                  <ilvl val="{level}"/>
-                  <numId val="{num_id}"/>
-                </numPr>
-              </pPr>
-              <r><t>{content}</t></r>
-            </p>
-        '''
-
+    def test_only_root_level_upperRoman_list_converted_to_heading(self):
         document_xml = ''.join([
-            list_item.format(
+            self.list_item.format(
                 content='AAA',
                 num_id=1,
                 level=0,
             ),
-            list_item.format(
+            self.list_item.format(
                 content='BBB',
                 num_id=1,
                 level=1,
             ),
-            list_item.format(
+            self.list_item.format(
                 content='CCC',
                 num_id=1,
                 level=2,
@@ -93,6 +92,129 @@ class ConvertRootUpperRomanListToHeadingTestCase(DocumentGeneratorTestCase):
                         <li>CCC</li>
                     </ol>
                 </li>
+            </ol>
+        '''
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_missing_numbering_definition(self):
+        document_xml = ''.join([
+            self.list_item.format(
+                content='AAA',
+                num_id=1,
+                level=0,
+            ),
+        ])
+
+        numbering_xml = ''
+
+        document = WordprocessingDocumentFactory()
+        document.add(NumberingDefinitionsPart, numbering_xml)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '''
+            <ol class="pydocx-list-style-type-None">
+                <li>AAA</li>
+            </ol>
+        '''
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_missing_numbering_part(self):
+        document_xml = ''.join([
+            self.list_item.format(
+                content='AAA',
+                num_id=1,
+                level=0,
+            ),
+        ])
+
+        document = WordprocessingDocumentFactory()
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '<p>AAA</p>'
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_missing_level_definition(self):
+        document_xml = ''.join([
+            self.list_item.format(
+                content='AAA',
+                num_id=1,
+                level=0,
+            ),
+        ])
+
+        numbering_xml = '''
+            <num numId="1">
+              <abstractNumId val="1"/>
+            </num>
+            <abstractNum abstractNumId="1">
+            </abstractNum>
+        '''
+
+        document = WordprocessingDocumentFactory()
+        document.add(NumberingDefinitionsPart, numbering_xml)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '''
+            <ol class="pydocx-list-style-type-None">
+                <li>AAA</li>
+            </ol>
+        '''
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_root_level_upperRoman_list_not_converted_to_heading(self):
+        document_xml = ''.join([
+            self.list_item.format(
+                content='AAA',
+                num_id=1,
+                level=0,
+            ),
+        ])
+
+        numbering_xml = '''
+            <num numId="1">
+              <abstractNumId val="1"/>
+            </num>
+            <abstractNum abstractNumId="1">
+              <lvl ilvl="0">
+                <numFmt val="upperRoman"/>
+              </lvl>
+            </abstractNum>
+        '''
+
+        document = WordprocessingDocumentFactory()
+        document.add(NumberingDefinitionsPart, numbering_xml)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '<h2>AAA</h2>'
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_root_level_decimal_list_not_converted_to_heading(self):
+        document_xml = ''.join([
+            self.list_item.format(
+                content='AAA',
+                num_id=1,
+                level=0,
+            ),
+        ])
+
+        numbering_xml = '''
+            <num numId="1">
+              <abstractNumId val="1"/>
+            </num>
+            <abstractNum abstractNumId="1">
+              <lvl ilvl="0">
+                <numFmt val="decimal"/>
+              </lvl>
+            </abstractNum>
+        '''
+
+        document = WordprocessingDocumentFactory()
+        document.add(NumberingDefinitionsPart, numbering_xml)
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '''
+            <ol class="pydocx-list-style-type-decimal">
+                <li>AAA</li>
             </ol>
         '''
         self.assert_document_generates_html(document, expected_html)

891d24251c289bf8d0e907885be0a8a2e9904a91
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-22 22:54:23 -0400

    Refs #147. Added Paragraph XmlModel

---
+++ pydocx/openxml/wordprocessing/paragraph.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+)
+
+from pydocx.models import XmlModel, XmlCollection, XmlChild
+from pydocx.openxml.wordprocessing.paragraph_properties import ParagraphProperties  # noqa
+from pydocx.openxml.wordprocessing.run import Run
+
+
+class Paragraph(XmlModel):
+    XML_TAG = 'p'
+
+    properties = XmlChild(type=ParagraphProperties)
+
+    children = XmlCollection(
+        Run,
+    )
+
+    def __iter__(self):
+        for node in self.children:
+            yield node

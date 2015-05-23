fcb52e4cabbd567a9df1b6fdf16a65a0b80e60a6
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-22 22:53:29 -0400

    Refs #147. Added Body XmlModel

---
+++ pydocx/openxml/wordprocessing/body.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+)
+
+from pydocx.models import XmlModel, XmlCollection
+from pydocx.openxml.wordprocessing.paragraph import Paragraph
+
+
+class Body(XmlModel):
+    XML_TAG = 'body'
+
+    children = XmlCollection(
+        Paragraph,
+    )
+
+    def __iter__(self):
+        for node in self.children:
+            yield node

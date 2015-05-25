9d4b2f9b5e8f76e1d46b042c0c8779c689a06d04
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-05-25 13:15:30 -0400

    Revert "Refs #148. next_child/previous_child implementation for the Body class"
    This reverts commit 93945c198a9241d183e67ae3a2837edfa2795901.

--- pydocx/openxml/wordprocessing/body.py
+++ pydocx/openxml/wordprocessing/body.py
@@ -16,31 +16,6 @@ class Body(XmlModel):
         Paragraph,
     )
 
-    def __init__(self, **kwargs):
-        super(Body, self).__init__(**kwargs)
-        self._children_lookup = dict(
-            (child, i)
-            for i, child in enumerate(self.children)
-        )
-
     def __iter__(self):
         for node in self.children:
             yield node
-
-    def next_child(self, child):
-        index = self._children_lookup.get(child, None)
-        if index is None:
-            return
-        next_child_index = index + 1
-        if next_child_index >= len(self.children):
-            return
-        return self.children[next_child_index]
-
-    def previous_child(self, child):
-        index = self._children_lookup.get(child, None)
-        if index is None:
-            return
-        previous_child_index = index - 1
-        if previous_child_index < 0:
-            return
-        return self.children[previous_child_index]

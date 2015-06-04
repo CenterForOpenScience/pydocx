44995adece03e998463e4dfa97c4726525732e63
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-06-04 16:39:41 -0400

    Refs #151. Comment is no longer a TODO

--- pydocx/openxml/wordprocessing/table_cell_properties.py
+++ pydocx/openxml/wordprocessing/table_cell_properties.py
@@ -13,12 +13,12 @@ class TableCellProperties(XmlModel):
 
     grid_span = XmlChild(name='gridSpan', attrname='val')
 
-    # TODO If vMerge is omitted, then this cell shall not be part of any
-    # vertically merged grouping of cells, and any vertically merged group of
-    # preceding cells shall be closed.
     vertical_merge = XmlChild(name='vMerge', type=lambda el: dict(el.attrib))  # noqa
 
     def should_close_previous_vertical_merge(self):
+        # If vMerge is omitted, then this cell shall not be part of any
+        # vertically merged grouping of cells, and any vertically merged group
+        # of preceding cells shall be closed.
         if self.vertical_merge is None:
             return True
         merge = self.vertical_merge.get('val', 'continue')

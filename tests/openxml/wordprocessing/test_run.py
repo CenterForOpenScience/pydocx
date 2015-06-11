9310b2daf46135538405d6a7d121a618c4a59dbb
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-06-11 17:15:47 -0400

    Refs #151. Added test case to show effective properties is memoized

--- tests/openxml/wordprocessing/test_run.py
+++ tests/openxml/wordprocessing/test_run.py
@@ -14,3 +14,10 @@ class RunTestCase(TestCase):
     def test_can_instantiate_empty_object(self):
         run = Run()
         assert run
+
+    def test_effective_properties_is_memoized(self):
+        run = Run()
+        effective_properties = run.effective_properties
+        sentinel = object()
+        effective_properties.bold = sentinel
+        self.assertEqual(run.effective_properties.bold, sentinel)

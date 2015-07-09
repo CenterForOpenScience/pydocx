8eabdfefe418bb58fca54d31498bb7861f27fb42
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-07-09 12:06:16 -0400

    Refs #134. Added test cases on start_margin_position

--- tests/openxml/wordprocessing/test_paragraph_properties.py
+++ tests/openxml/wordprocessing/test_paragraph_properties.py
@@ -11,11 +11,13 @@ from pydocx.openxml.wordprocessing import NumberingProperties, ParagraphProperti
 from pydocx.util.xml import parse_xml_from_string
 
 
-class ParagraphPropertiesTestCase(TestCase):
+class ParagraphPropertiesTestBase(TestCase):
     def _load_from_xml(self, xml):
         root = parse_xml_from_string(xml)
         return ParagraphProperties.load(root)
 
+
+class ParagraphPropertiesTestCase(ParagraphPropertiesTestBase):
     def test_pStyle_child_mapped_to_parent_style_attribute(self):
         xml = '''
             <pPr>
@@ -60,3 +62,46 @@ class ParagraphPropertiesTestCase(TestCase):
         '''
         properties = self._load_from_xml(xml)
         self.assertEqual(properties.indentation_first_line, '789')
+
+
+class StartMarginPositionTestCase(ParagraphPropertiesTestBase):
+    def test_returns_zero_when_no_indentation_set(self):
+        xml = '<pPr />'
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.start_margin_position, 0)
+
+    def test_returns_left_indentation_by_itself(self):
+        xml = '''
+            <pPr>
+                <ind left="123" />
+            </pPr>
+        '''
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.start_margin_position, 123)
+
+    def test_returns_left_minus_hanging(self):
+        xml = '''
+            <pPr>
+                <ind left="123" hanging="23" />
+            </pPr>
+        '''
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.start_margin_position, 100)
+
+    def test_returns_left_plus_first_line(self):
+        xml = '''
+            <pPr>
+                <ind left="123" firstLine="50" />
+            </pPr>
+        '''
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.start_margin_position, 173)
+
+    def test_returns_left_minus_hanging_ignoring_first_line(self):
+        xml = '''
+            <pPr>
+                <ind left="123" hanging="23" firstLine="50" />
+            </pPr>
+        '''
+        properties = self._load_from_xml(xml)
+        self.assertEqual(properties.start_margin_position, 100)

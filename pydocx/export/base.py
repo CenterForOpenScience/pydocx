ca0feb0c42d5f00353ee4186da68643167bf4729
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-08-04 15:05:03 -0400

    Refs #134. Refactor to make it easier to override the numbering span builder class

--- pydocx/export/base.py
+++ pydocx/export/base.py
@@ -20,6 +20,8 @@ from pydocx.openxml.packaging import WordprocessingDocument
 
 
 class PyDocXExporter(object):
+    numbering_span_builder_class = NumberingSpanBuilder
+
     def __init__(self, path):
         self.path = path
         self._document = None
@@ -161,7 +163,7 @@ class PyDocXExporter(object):
                 previous_was_empty = empty
 
     def yield_numbering_spans(self, items):
-        builder = NumberingSpanBuilder(items)
+        builder = self.numbering_span_builder_class(items)
         numbering_spans = builder.get_numbering_spans()
         for item in numbering_spans:
             yield item

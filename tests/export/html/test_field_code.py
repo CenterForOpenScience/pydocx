c9b589d73f0ac402d42f0f5f8b585721c67c16d8
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-09-16 17:37:41 -0400

    Refs #72. Added another failing hyperlink test: one that spans multiple paragraphs

--- tests/export/html/test_field_code.py
+++ tests/export/html/test_field_code.py
@@ -13,7 +13,7 @@ from pydocx.test.utils import WordprocessingDocumentFactory
 
 
 class FieldCodeTestCase(DocumentGeneratorTestCase):
-    def test_hyperlink(self):
+    def test_hyperlink_within_single_paragraph(self):
         document_xml = '''
             <p>
                 <r><t>Link: </t></r>
@@ -39,4 +39,44 @@ class FieldCodeTestCase(DocumentGeneratorTestCase):
         document.add(MainDocumentPart, document_xml)
 
         expected_html = '<p>Link: <a href="http://www.google.com">AAA</a></p>'
+        self.assert_document_generates_html(document, expected_html)
+
+    def test_hyperlink_spanning_multiple_paragraphs(self):
+        document_xml = '''
+            <p>
+                <r><t>Link: </t></r>
+            </p>
+            <p>
+                <r>
+                    <fldChar fldCharType="begin"/>
+                </r>
+                <r>
+                    <instrText> HYPERLINK "https://www.google.com/"</instrText>
+                </r>
+                <r>
+                    <fldChar fldCharType="separate"/>
+                </r>
+                <r>
+                    <t>AAA</t>
+                </r>
+                <r>
+                    <t>BBB</t>
+                </r>
+            </p>
+            <p><r><t>CCC</t></r></p>
+            <p>
+                <r>
+                    <fldChar fldCharType="end"/>
+                </r>
+                <r><t>.</t></r>
+            </p>
+        '''
+        document = WordprocessingDocumentFactory()
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '''
+            <p>Link: </p>
+            <p><a href="http://www.google.com">AAABBB</a></p>
+            <p><a href="http://www.google.com">CCC</a>.</p>
+        '''
         self.assert_document_generates_html(document, expected_html)

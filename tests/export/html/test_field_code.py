73dc1e8ab71f608aa8ff728db38727c795cc4fe2
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-09-17 12:24:31 -0400

    Refs #72. Added failing test case to describe 17.16.18

--- tests/export/html/test_field_code.py
+++ tests/export/html/test_field_code.py
@@ -80,3 +80,32 @@ class FieldCodeTestCase(DocumentGeneratorTestCase):
             <p><a href="http://www.google.com">CCC</a>.</p>
         '''
         self.assert_document_generates_html(document, expected_html)
+
+    def test_missing_fld_char_end(self):
+        # According to 17.16.18, "If a complex field is not closed before the
+        # end of a document story, then no field shall be generated and each
+        # individual run shall be processed as if the field characters did not
+        # exist"
+        document_xml = '''
+            <p>
+                <r><t>Link: </t></r>
+                <r>
+                    <fldChar fldCharType="begin"/>
+                </r>
+                <r>
+                    <instrText> HYPERLINK "https://www.google.com/"</instrText>
+                </r>
+                <r>
+                    <fldChar fldCharType="separate"/>
+                </r>
+                <r>
+                    <t>AAA</t>
+                </r>
+                <r><t>.</t></r>
+            </p>
+        '''
+        document = WordprocessingDocumentFactory()
+        document.add(MainDocumentPart, document_xml)
+
+        expected_html = '<p>Link: AAA.</p>'
+        self.assert_document_generates_html(document, expected_html)

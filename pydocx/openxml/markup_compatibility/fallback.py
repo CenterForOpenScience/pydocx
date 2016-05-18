a58da52c3ddffbf40db9ca8af52f9faf56f18d14
Jason Ward <jason.ward@policystat.com>
2016-05-18 17:08:45 -0400

    refs #204: Added a comment.

--- pydocx/openxml/markup_compatibility/fallback.py
+++ pydocx/openxml/markup_compatibility/fallback.py
@@ -10,4 +10,9 @@ from pydocx.models import XmlModel, XmlCollection
 
 class Fallback(XmlModel):
     XML_TAG = 'Fallback'
+
+    # It would be better to refer to the grandparent's children XmlCollection
+    # lazily. The problem is that we don't have a good way to represent lazy
+    # fields, nor do we have a way for those fields to reference their parents
+    # before they are initialized with content.
     children = XmlCollection(allow_all_children=True)

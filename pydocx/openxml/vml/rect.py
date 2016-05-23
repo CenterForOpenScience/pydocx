4c665f3dc20d187dfdc1feb77eb65e63ed4da471
Jason Ward <jason.ward@policystat.com>
2016-05-23 15:09:11 -0400

    refs #218: Fixed the rect tag.

--- pydocx/openxml/vml/rect.py
+++ pydocx/openxml/vml/rect.py
@@ -5,11 +5,22 @@ from __future__ import (
     unicode_literals,
 )
 
-from pydocx.models import XmlModel, XmlCollection
+from pydocx.models import XmlModel, XmlCollection, XmlAttribute
 from pydocx.openxml.vml.image_data import ImageData
 
 
 class Rect(XmlModel):
     XML_TAG = 'rect'
 
+    style = XmlAttribute()
     children = XmlCollection(ImageData, 'vml.Textbox')
+
+    # TODO perhaps we could have a prepare_style, or clean_style convention?
+    def get_style(self):
+        if self.style:
+            return dict(
+                item.split(':', 1)
+                for item in self.style.split(';')
+                if item
+            )
+        return {}
